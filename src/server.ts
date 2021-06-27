22d1580c07d7ae8010316611f22d50270cd7150a
Benício Barreto <benicio.daniel@gmail.com>
2021-06-27 14:31:18 -0300

    trying to fix access control allow origin issue

--- src/server.ts
+++ src/server.ts
@@ -10,4 +10,14 @@ app.use(cors());
 app.use(express.json());
 app.use(routes);
 
+app.use(function (req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+  res.header(
+    'Access-Control-Allow-Headers',
+    'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json'
+  );
+  next();
+});
+
 app.listen(process.env.PORT || 3333, () => console.log('server running'));
